d79817141d70805e51ddaaa9a29054b0c6675ab1
aishwaryaa03 <102683520+aishwaryaa03@users.noreply.github.com>
2023-04-25 13:03:27 +0530

    Update Bike.java

--- day1/Bike.java
+++ day1/Bike.java
@@ -2,11 +2,21 @@ package week1.day1;
 
 public class Bike {
 
+	public void applyBreak() {
+		System.out.println("Applied Break for Bike");
+	}
+	
+	public void soundHorn() {
+		System.out.println("Sound Horn for Bike");
+	}
+	
 	public static void main(String[] args) {
            Car obj = new Car();
            obj.applyBreak();
            obj.soundHorn();
            Bike obj1 = new Bike();
+           obj1.applyBreak();
+           obj1.soundHorn();
 	}
 
 }
